26be9fd1fcc811255305ec860a40f4030da3bc41
Jan <yoxutv@gmail.com>
2019-09-26 21:16:37 +0200

    Moving works

--- Old - main.py
+++ Old - main.py
@@ -5,52 +5,40 @@ from os import path
 from os import system
 
 
-
-
-
 class main(object):
     def __init__(self):
-        self.screen = pg.display.set_mode((860,658))
+        self.screen = pg.display.set_mode((860, 640))
         self.loadData()
         self.firstMakeMatrix()
         pg.init()
         pg.display.set_caption("Game")
 
-
-        self.active = False
+        self.selected = []
 
 
         while True:
+            self.draw()
             self.screen.fill((0, 0, 0))
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     sys.exit(0)
 
-            self.pos = pg.mouse.get_pos()
-            pressed1, pressed2, pressed3 = pg.mouse.get_pressed()
+                if event.type == pg.MOUSEBUTTONDOWN and pg.mouse.get_pos()[0] / 80 < 8:
+                    self.hor = pg.mouse.get_pos()[0] / 80
+                    self.ver = pg.mouse.get_pos()[1] / 80
 
-            for event in pg.event.get():
-                if event.type == pg.MOUSEBUTTONUP: self.active = False
-
-
-
-            self.draw()
-
-            #if pressed1 or pressed3: self.active = False
-
-
-            if pressed1:
-
-                if self.drawLabel[3][1].collidepoint(self.pos) and pressed1 and self.active:
-                    self.moveFigure(0, 3, 3, 1)
-                    self.active = False
-
-                    print("lol")
+                    self.selected.insert(0,{"h":self.hor,"v":self.ver,"t":self.matrix[int(self.ver)][int(self.hor)]})
+                    try:
+                        print("1:",self.selected[1].get("h"), self.selected[1].get("v"))
+                        print("2:",self.selected[0].get("h"), self.selected[0].get("v"))
+                        self.moveFigure(int(self.selected[1].get("v")),int(self.selected[1].get("h")),int(self.selected[0].get("v")),int(self.selected[0].get("h")))
 
 
 
-                if self.K1.collidepoint(self.pos) and pressed1 or self.active:
-                    self.active = True
+
+
+
+                    except: pass
 
 
 
@@ -58,65 +46,67 @@ class main(object):
 
 
 
-            pg.display.flip()
-
-
-    def moveFigure(self,fr1,fr2,to1,to2):
+    def moveFigure(self, fr1, fr2, to1, to2):
         if fr1 != to1 or fr2 != to2:
             if self.matrix[fr1][fr2] != 0:
-                self.matrix[to2][to1] = self.matrix[fr1][fr2]
+                self.matrix[to1][to2\\] = self.matrix[fr1][fr2]
                 self.matrix[fr1][fr2] = 0
 
-
-
     def draw(self):
-        pg.draw.rect(self.screen, (112, 41, 0), pg.Rect(0, 0, 658, 658))
 
-        colorid=0
+        colorid = 0
         for i in range(8):
             colorid += 1
             for j in range(8):
-                if colorid%2 == 0: color = (90, 90, 90)
-                else: color = (220, 220, 220)
+                if colorid % 2 == 0:
+                    color = (90, 90, 90)
+                else:
+                    color = (220, 220, 220)
                 colorid += 1
-                self.drawLabel[i][j] = pg.draw.rect(self.screen, (color), pg.Rect(i*82+2, j*82+2, 80, 80))
+                self.drawLabel[i][j] = pg.draw.rect(self.screen, (color), pg.Rect(i * 80, j * 80, 80, 80))
 
         for i in range(8):
             for j in range(8):
-                if self.matrix[i][j] == "P1": self.P1 = self.screen.blit(self.figureP, (j*82+2, i*82+2))
-                if self.matrix[i][j] == "P2": self.P2 = self.screen.blit(self.figureP, (j*82+2, i*82+2))
-                if self.matrix[i][j] == "P3": self.P2 = self.screen.blit(self.figureP, (j*82+2, i*82+2))
-                if self.matrix[i][j] == "P4": self.P2 = self.screen.blit(self.figureP, (j*82+2, i*82+2))
-                if self.matrix[i][j] == "P5": self.P2 = self.screen.blit(self.figureP, (j*82+2, i*82+2))
-                if self.matrix[i][j] == "P6": self.P2 = self.screen.blit(self.figureP, (j*82+2, i*82+2))
-                if self.matrix[i][j] == "P7": self.P2 = self.screen.blit(self.figureP, (j*82+2, i*82+2))
-                if self.matrix[i][j] == "P8": self.P2 = self.screen.blit(self.figureP, (j*82+2, i*82+2))
-                if self.matrix[i][j] == "PC1": self.PC1 = self.screen.blit(self.figurePC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "PC2": self.PC2 = self.screen.blit(self.figurePC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "PC3": self.PC2 = self.screen.blit(self.figurePC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "PC4": self.PC2 = self.screen.blit(self.figurePC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "PC5": self.PC2 = self.screen.blit(self.figurePC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "PC6": self.PC2 = self.screen.blit(self.figurePC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "PC7": self.PC2 = self.screen.blit(self.figurePC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "PC8": self.PC2 = self.screen.blit(self.figurePC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "K": self.K1 = self.screen.blit(self.figureK, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "KC": self.KC1 = self.screen.blit(self.figureKC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "H": self.H1 = self.screen.blit(self.figureH, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "HC": self.HC1 = self.screen.blit(self.figureHC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "W1": self.W1 = self.screen.blit(self.figureW, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "W2": self.W2 = self.screen.blit(self.figureW, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "WC1": self.WC1 = self.screen.blit(self.figureWC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "WC2": self.WC2 = self.screen.blit(self.figureWC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "G1": self.G1 = self.screen.blit(self.figureG, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "G2": self.G2 = self.screen.blit(self.figureG, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "GC1": self.GC1 = self.screen.blit(self.figureGC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "GC2": self.GC2 = self.screen.blit(self.figureGC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "S1": self.S1 = self.screen.blit(self.figureS, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "S2": self.S2 = self.screen.blit(self.figureS, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "SC1": self.SC1 = self.screen.blit(self.figureSC, (j * 82 + 2, i * 82 + 2))
-                if self.matrix[i][j] == "SC2": self.SC2 = self.screen.blit(self.figureSC, (j * 82 + 2, i * 82 + 2))
+                if self.matrix[i][j] == "P1": self.P1 = self.screen.blit(self.figureP, (j * 80, i * 80))
+                if self.matrix[i][j] == "P2": self.P2 = self.screen.blit(self.figureP, (j * 80, i * 80))
+                if self.matrix[i][j] == "P3": self.P3 = self.screen.blit(self.figureP, (j * 80, i * 80))
+                if self.matrix[i][j] == "P4": self.P4 = self.screen.blit(self.figureP, (j * 80, i * 80))
+                if self.matrix[i][j] == "P5": self.P5 = self.screen.blit(self.figureP, (j * 80, i * 80))
+                if self.matrix[i][j] == "P6": self.P6 = self.screen.blit(self.figureP, (j * 80, i * 80))
+                if self.matrix[i][j] == "P7": self.P7 = self.screen.blit(self.figureP, (j * 80, i * 80))
+                if self.matrix[i][j] == "P8": self.P8 = self.screen.blit(self.figureP, (j * 80, i * 80))
+                if self.matrix[i][j] == "PC1": self.PC1 = self.screen.blit(self.figurePC, (j * 80, i * 80))
+                if self.matrix[i][j] == "PC2": self.PC2 = self.screen.blit(self.figurePC, (j * 80, i * 80))
+                if self.matrix[i][j] == "PC3": self.PC3 = self.screen.blit(self.figurePC, (j * 80, i * 80))
+                if self.matrix[i][j] == "PC4": self.PC4 = self.screen.blit(self.figurePC, (j * 80, i * 80))
+                if self.matrix[i][j] == "PC5": self.PC5 = self.screen.blit(self.figurePC, (j * 80, i * 80))
+                if self.matrix[i][j] == "PC6": self.PC6 = self.screen.blit(self.figurePC, (j * 80, i * 80))
+                if self.matrix[i][j] == "PC7": self.PC7 = self.screen.blit(self.figurePC, (j * 80, i * 80))
+                if self.matrix[i][j] == "PC8": self.PC8 = self.screen.blit(self.figurePC, (j * 80, i * 80))
+                if self.matrix[i][j] == "K": self.K1 = self.screen.blit(self.figureK, (j * 80, i * 80))
+                if self.matrix[i][j] == "KC": self.KC1 = self.screen.blit(self.figureKC, (j * 80, i * 80))
+                if self.matrix[i][j] == "H": self.H1 = self.screen.blit(self.figureH, (j * 80, i * 80))
+                if self.matrix[i][j] == "HC": self.HC1 = self.screen.blit(self.figureHC, (j * 80, i * 80))
+                if self.matrix[i][j] == "W1": self.W1 = self.screen.blit(self.figureW, (j * 80, i * 80))
+                if self.matrix[i][j] == "W2": self.W2 = self.screen.blit(self.figureW, (j * 80, i * 80))
+                if self.matrix[i][j] == "WC1": self.WC1 = self.screen.blit(self.figureWC, (j * 80, i * 80))
+                if self.matrix[i][j] == "WC2": self.WC2 = self.screen.blit(self.figureWC, (j * 80, i * 80))
+                if self.matrix[i][j] == "G1": self.G1 = self.screen.blit(self.figureG, (j * 80, i * 80))
+                if self.matrix[i][j] == "G2": self.G2 = self.screen.blit(self.figureG, (j * 80, i * 80))
+                if self.matrix[i][j] == "GC1": self.GC1 = self.screen.blit(self.figureGC, (j * 80, i * 80))
+                if self.matrix[i][j] == "GC2": self.GC2 = self.screen.blit(self.figureGC, (j * 80, i * 80))
+                if self.matrix[i][j] == "S1": self.S1 = self.screen.blit(self.figureS, (j * 80, i * 80))
+                if self.matrix[i][j] == "S2": self.S2 = self.screen.blit(self.figureS, (j * 80, i * 80))
+                if self.matrix[i][j] == "SC1": self.SC1 = self.screen.blit(self.figureSC, (j * 80, i * 80))
+                if self.matrix[i][j] == "SC2": self.SC2 = self.screen.blit(self.figureSC, (j * 80, i * 80))
+                if pg.mouse.get_pos()[0] / 80 < 8: pg.draw.rect(self.screen, (100, 120, 200),
+                             pg.Rect(int(pg.mouse.get_pos()[0] / 80) * 80, int(pg.mouse.get_pos()[1] / 80) * 80, 80,
+                                     80), 4)
+        try:
+            pg.draw.rect(self.screen, (200, 40, 40), pg.Rect(int(self.hor) * 80, int(self.ver) * 80, 80, 80), 2)
+        except: pass
 
-
+        pg.display.flip()
 
     def loadData(self):
         img_dir = path.join(path.dirname(__file__), "data/")
@@ -132,7 +122,6 @@ class main(object):
         self.figureSC = pg.image.load(img_dir + "sc.png")
         self.figureH = pg.image.load(img_dir + "h.png")
         self.figureHC = pg.image.load(img_dir + "hc.png")
-
 
     def firstMakeMatrix(self):
         self.matrix = []
@@ -154,20 +143,20 @@ class main(object):
         self.matrix[1][0] = "P1"
         self.matrix[1][1] = "P2"
         self.matrix[1][2] = "P3"
-        self.matrix[1][0] = "P4"
+        self.matrix[1][3] = "P4"
         self.matrix[1][4] = "P5"
         self.matrix[1][5] = "P6"
         self.matrix[1][6] = "P7"
         self.matrix[1][7] = "P8"
 
-        self.matrix[6][0] = "P1"
-        self.matrix[6][1] = "P2"
-        self.matrix[6][2] = "P3"
-        self.matrix[6][3] = "P4"
-        self.matrix[6][4] = "P5"
-        self.matrix[6][5] = "P6"
-        self.matrix[6][6] = "P7"
-        self.matrix[6][7] = "P8"
+        self.matrix[6][0] = "PC1"
+        self.matrix[6][1] = "PC2"
+        self.matrix[6][2] = "PC3"
+        self.matrix[6][3] = "PC4"
+        self.matrix[6][4] = "PC5"
+        self.matrix[6][5] = "PC6"
+        self.matrix[6][6] = "PC7"
+        self.matrix[6][7] = "PC8"
         self.matrix[7][0] = "WC1"
         self.matrix[7][1] = "SC1"
         self.matrix[7][2] = "GC1"
@@ -184,11 +173,6 @@ class main(object):
 
         for i in range(len(self.matrix)):
             print(self.matrix[i])
-
-
-
-
-
 
 
 if __name__ == "__main__":
